edd08c7ecf85770211b732a913dc99427d9b0d72
christianleng <christian.leng@epitech.eu>
2025-11-28 15:18:32 +0100

    Dice Rolling Game

---
+++ Dice Rolling Game.py
@@ -0,0 +1,21 @@
+import random
+
+# range (start, stop, step)
+
+
+def GenerateRandomNumber():
+    for x in range(1):
+        print(f"({random.randint(1, 6)}, {random.randint(1, 6)})")
+
+
+def DiceRollingGame():
+    while True:
+        GenerateRandomNumber()
+        ShouldRollAgain = input("Roll the dice? (y/n): ")
+        if ShouldRollAgain.lower() == "y":
+            DiceRollingGame()
+        else:
+            break
+
+
+DiceRollingGame()
