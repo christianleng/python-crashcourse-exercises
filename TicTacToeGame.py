eae510e3fc99ae53a2e383a592b2382d49619985
christianleng <christian.leng@epitech.eu>
2025-12-04 19:17:20 +0100

    tictactoe

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,11 +1,11 @@
-board = [
+boards = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "],
 ]
 
 
-def print_board(rows: list[list[str]]):
+def print_board(rows: list[list[str]]) -> None:
     horizontal = "---+---+---"
     for i in range(3):
         print(horizontal)
@@ -18,23 +18,59 @@ def print_board(rows: list[list[str]]):
     print(horizontal)
 
 
+def check_winner(boards: list[list[str]]) -> None | bool:
+    for i in range(3):
+        if boards[i][0] == boards[i][1] == boards[i][2] != " ":
+            return True
+
+    for j in range(3):
+        if boards[0][j] == boards[1][j] == boards[2][j] != " ":
+            return True
+
+    for k in range(3):
+        if boards[0][0] == boards[1][1] == boards[2][2] != " ":
+            return True
+
+    for d in range(3):
+        if boards[0][2] == boards[1][1] == boards[2][0] != " ":
+            return True
+
+    return None
+
+
 current_player = "X"
 
 
-def check_winner():
-    global current_player
+def check_draw(boards: list[list[str]]) -> bool:
+    for i in range(3):
+        for j in range(3):
+            if boards[i][j] == " ":
+                return False
+    return True
 
+
+while True:
     print(f"Player {current_player}'s turn")
     row = int(input("Enter row (0-2): "))
     column = int(input("Enter column (0-2): "))
 
-    if board[row][column] != " ":
-        print("Already")
-        return
+    if boards[row][column] != " ":
+        print("Already take")
+        continue
 
-    board[row][column] = current_player
+    boards[row][column] = current_player
 
-    print_board(board)
+    print_board(boards)
 
+    has_winner = check_winner(boards)
+    has_no_winner = check_draw(boards)
 
-check_winner()
+    if has_winner != None:
+        print(f"WINNER : {current_player}")
+        break
+
+    if has_no_winner:
+        print(f"DRAW !")
+        break
+
+    current_player = "O" if current_player == "X" else "X"
